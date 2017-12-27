92dd7e8efbf28631f3df054c9aacbc3b526f9cc7
seddieDwamena <seddieDwamena>
2017-12-27 10:57:40 +0000

    x

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
 $( document ).ready(function(){
     let height = $(window).height();
     let width = $(window).width();
+    
+
+    // $('.carousel').carousel();
+
+    if(width>768){
+        $("#homepage-section3").css("height",height); 
+        $("#homepage-section2").css("height",height);
+        $("#homepage-section1").css("height",height);
+        $("#homepage-section1 img").css("height",height);
+    }
+    if(width < 500){
+        $("#homepage-section1").css("height",height/2); 
+    }
 
     console.log(height);
+    $("body").css("width",width)
     $("#get-form").css("width",width);
     $("#get-form").css("height",height);
-    $("#homepage-section2").css("height",height);
-    $("#homepage-section3").css("height",height);
-    $("#homepage-section1 img").css("height",height);
+    
+    //$("#homepage-section3").css("height",height);
+    $("#homepage-section1 img").css("height","100%");
     $(".button-collapse").sideNav();
     $('ul.tabs').tabs();
     $('.carousel.carousel-slider').carousel({
@@ -21,6 +35,7 @@ $( document ).ready(function(){
     }
 
     AOS.init({
-        duration: 3000
+        duration: 3000,
+        disable: 'mobile'
     })
 })
